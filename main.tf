0c40bcc0f84e2196c4ab965810dce46fe76346c4
marina <m.popil@itoutposts.com>
2024-01-15 13:55:49 +0200

    add helm

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ provider "google" {
 resource "google_container_cluster" "my_cluster" {
   name     = "my-cluster"
   location = "us-central1-a"
+  deletion_protection=false
 
   node_config {
     machine_type = "e2-medium"
@@ -29,7 +30,7 @@ resource "google_container_node_pool" "infra_pool" {
     image_type   = "COS_CONTAINERD"
   }
 
-  initial_node_count = 2
+  initial_node_count = 1
 }
 
 resource "google_container_node_pool" "app_pool" {
@@ -43,6 +44,45 @@ resource "google_container_node_pool" "app_pool" {
     image_type   = "COS_CONTAINERD"
   }
 
-  initial_node_count = 2
+  initial_node_count = 1
+}
+resource "helm_release" "elasticsearch" {
+  name       = "elasticsearch"
+  repository = "https://helm.elastic.co"
+  chart      = "elasticsearch"
+  version    = "8.5.1"
+
+  set {
+    name  = "clusterName"
+    value = "my-cluster"
+  }
+}
+  set {
+  name  = "volumeClaimTemplate.resources.requests.storage"
+  value = "5Gi"
+}
+
+resource "kubernetes_storage_class" "elasticsearch_ssd" {
+  metadata {
+    name = "elasticsearch-ssd"
+  }
+  storage_provisioner = "kubernetes.io/gce-pd"
+  reclaim_policy      = "Retain"
+  parameters = {
+    type = "pd-ssd"
+  }
+  allow_volume_expansion = true
+}
+
+resource "helm_release" "kibana" {
+  name       = "kibana"
+  repository = "https://helm.elastic.co"
+  chart      = "kibana"
+  version    = "8.5.1"
+
+  set {
+    name  = "elasticsearch.hosts"
+    value = "http://exampledomain27.pp.ua:9200"  
+  }
 }
 
